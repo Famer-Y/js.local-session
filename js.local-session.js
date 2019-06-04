94d873d16e9a43bf32f6fd0548a25f7ca20fcb50
Famer <1243204339@qq.com>
2019-06-04 11:50:19 +0800

    version 3

--- js.local-session.js
+++ js.local-session.js
@@ -1,43 +1,132 @@
-var LS = function(type){
-    var self = this;
-    self._storage = (type === 'session' ? window.sessionStorage : window.localStorage);
+// project : js.local-session
 
-    self.set = function(name, value) {
-        try {
-            if (isUndifined(name) || isUndifined(value)) {
-                throw new Error("name or value is not defined!!!");
+// github  : https://github.com/Famer-Y/js.local-session
+
+;(function(root, factory){
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if ( typeof module === "object" && typeof module.exports === "object" ) {
+        module.exports = factory(root);
+    } else {
+        root.storage = factory(window);
+    }
+}(typeof window !== "undefined" ? window : this, function(window){
+    var ls = window.localStorage;
+    var storage = {
+        set: function(){
+            if (0 === arguments.length) {
+                throw new Error("1 or 2 arguments required, but only 0 present.");
             }
-            self._storage.setItem(name, value);
-        } catch (e){
-            console.error(e);
-        }
-    }
+            if (1 === arguments.length) {
+                var param = arguments[0];
+                if ("Object" !== analyzeType(param)) {
+                    throw new Error("arguments must be Object, when the number of arguments only 1.");
+                }
+                for (var item in param) {
+                    ls.setItem(item, JSON.stringify(param[item]));
+                }
+            }
+            if (2 === arguments.length) {
+                var name = arguments[0];
+                var value = arguments[1];
+                ls.setItem(name, JSON.stringify(value));
+            }
+        },
 
-    self.get = function(name) {
-        if (undefined === self._storage.getItem(name)) {
-            if (undefined !== defaultValue) {
-                self.set(name, defaultValue);
-                return defaultValue;
-            } else {
-                return null;
+        get: function(){
+            if (0 === arguments.length) {
+                var result = {};
+                for (var index = 0; index < ls.length; index ++) {
+                    var key = ls.key(index);
+                    try {
+                        result[key] = JSON.parse(ls.getItem(key));  
+                    } catch (e) {
+                        console.debug(e);
+                        result[key] = ls.getItem(key);
+                    }
+                }
+                return result;
+            }
+            if (arguments.length >= 1) {
+                console.log(arguments);
+                var name = arguments[0];
+                var defaultValue = arguments[1];
+                if (undefined === ls[name]) {
+                    console.log(defaultValue);
+                    if (undefined === defaultValue) {
+                        return null;
+                    } else {
+                        storage.set(name, defaultValue);
+                        return defaultValue;
+                    }
+                }
+                try {
+                    return JSON.parse(ls.getItem(name));
+                } catch (e) {
+                    console.debug(e);
+                    return ls.getItem(name);
+                }
+            }
+        },
+
+        regex: function(){
+            if (0 === arguments.length) {
+                throw new Error("1 argument required, but only 0 present.");
+            }
+            if (arguments.length >= 1) {
+                var pattern = arguments[0];
+                if (!(pattern instanceof RegExp)) {
+                    throw new Error("argument must be RegExp.");
+                }
+                var result = {};
+                for (var index = 0; index < ls.length; index ++) {
+                    var key = ls.key(index);
+                    if (pattern.test(key)) {
+                        try {
+                            result[key] = JSON.parse(ls.getItem(key));  
+                        } catch (e) {
+                            console.debug(e);
+                            result[key] = ls.getItem(key);
+                        }
+                    }
+                }
+                return result;
+            }
+        },
+
+        remove: function(){
+            if (0 === arguments.length) {
+                throw new Error("1 argument required, but only 0 present.");
+            }
+            if (1 === arguments.length) {
+                var param = arguments[0];
+                if ("Array" === analyzeType(param)) {
+                    for (var index = 0; index < param.length; index ++) {
+                        var name = param[index];
+                        ls.removeItem(name);
+                    }
+                } else if ("String" === analyzeType(param)) {
+                    ls.removeItem(param);
+                }
             }
         }
-        try {
-            return JSON.parse(self._storage.getItem(name));
-        } catch (e) {
-            self.set(name, defaultValue);
-            return defaultValue;
-        }
     }
 
-    self.remove = function(name) {
-        self._storage.removeItem(name)
-    }
-
-    function isUndifined(key){
-        if (key === undefined) {
-            return true;
+    function analyzeType(o){
+        switch (Object.prototype.toString.call(o)) {
+            case "[object Object]":
+                return "Object";
+            case "[object Array]":
+                return "Array";
+            case "[object Number]":
+                return "Number";
+            case "[object Boolean]":
+                return "Boolean";
+            case "[object String]":
+                return "String";
+            default:
+                return "undefined";
         }
-        return false;
     }
-}
+    return storage;
+}));
